e58b27953a2dd3f95c9bf9edfd94fddf7ea84411
remelop03 <remelop@unal.edu.co>
2025-02-10 03:10:31 +0000

    done

--- app.py
+++ app.py
@@ -26,3 +26,76 @@ import pandas as pd
 #print(df)
 
 #05.1 DataFrame Dict
+#data = [
+#    { 
+#        "brand": "Toyota", 
+#        "model": "Corolla",
+#        "color": "Blue"
+#    },
+#    {
+#        "brand": "Ford", 
+#        "model": "K",
+#        "color": "Yellow"
+#    },
+#    {
+#        "brand": "Porsche", 
+#        "model": "Cayenne",
+#        "color": "White"
+#    },
+#    {
+#        "brand": "Tesla", 
+#        "model": "Model S",
+#        "color": "Red"
+#    }
+#]
+#df=pd.DataFrame(data)
+#print(df)
+
+#05.2 DataFrame iLoc
+#data_frame=pd.read_csv(".learn/assets/pokemon_data.csv")
+#print(data_frame.iloc[133,6])
+
+#05.3 DataFrame Head
+#data_frame=pd.read_csv(".learn/assets/pokemon_data.csv")
+#print(data_frame.head(3))
+
+#05.4 DataFrame Tail
+#data_frame=pd.read_csv(".learn/assets/pokemon_data.csv")
+#print(data_frame.tail(3))
+
+#05.5 Print Columns
+#data_frame=pd.read_csv(".learn/assets/pokemon_data.csv")
+#data_frame=pd.DataFrame(data_frame)
+#print(data_frame[["Name","Type 1"]][0:10])
+
+#05.6 Loc Function
+#data_frame=pd.read_csv(".learn/assets/pokemon_data.csv")
+#data_frame=pd.DataFrame(data_frame)
+#print(data_frame.loc[data_frame['Attack'] > 80])
+
+#05.7 Filter and Count
+#data_frame=pd.read_csv(".learn/assets/pokemon_data.csv")
+#data_frame=pd.DataFrame(data_frame)
+#print(len(data_frame.loc[data_frame['Legendary'] == 1]))
+
+#06 Clean Datasets
+#df=pd.read_csv(".learn/assets/us_baby_names_right.csv")
+#df=pd.DataFrame(df)
+#print(df.head())
+
+#06.1 Remove Column
+#df=pd.read_csv(".learn/assets/us_baby_names_right.csv")
+#df=pd.DataFrame(df)
+#df = df.drop("Unnamed: 0", axis=1)
+#print(df.head())
+
+#06.2 Value Counts
+#df=pd.read_csv(".learn/assets/us_baby_names_right.csv")
+#df=pd.DataFrame(df)
+#print(df["Gender"].value_counts())
+
+#06.3 Group By
+df=pd.read_csv(".learn/assets/us_baby_names_right.csv")
+df=pd.DataFrame(df)
+print(len(df.groupby(['Name']).sum()))
+
